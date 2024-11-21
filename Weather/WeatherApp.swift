323b69c0dbe07cabbef1569c386f00e5d5b1cea0
Ciaran Mulholland <ciaranmul@googlemail.com>
2024-11-21 21:12:15 +0000

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ciarán Mulholland on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Ciarán Mulholland on 21/11/2024.
+//
+
+import Testing
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
